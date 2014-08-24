2a38b8a726a2cf2dbb89346540777eb4fb61bfca
Samir Faci <dev@esamir.com>
2014-08-24 09:49:37 -0700

    Initial Commit

---
+++ plugin.py
@@ -0,0 +1,114 @@
+###
+# Copyright (c) 2014, Samir Faci
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#   * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions, and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions, and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#   * Neither the name of the author of this software nor the name of
+#     contributors to this software may be used to endorse or promote products
+#     derived from this software without specific prior written consent.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+###
+
+import supybot.utils as utils
+from supybot.commands import *
+import supybot.plugins as plugins
+import supybot.ircutils as ircutils
+import supybot.callbacks as callbacks
+import supybot.httpserver as httpserver
+import supybot.ircmsgs as ircmsgs
+import supybot.world as world
+import re
+import tinyurl as t
+
+
+import feedparser
+import threading
+
+
+
+class Nixie(callbacks.Plugin):
+    """Add the help for "@plugin help Nixie" here
+    This should describe *how* to use this plugin."""
+    nixiedocuments = None
+
+    def __init__(self, irc):
+        self.__parent = super(Nixie, self)
+        self.__parent.__init__(irc)
+        self.nixie_rss = "http://feeds.feedburner.com/nixiepixel"
+        self.__create_nixiedocs()
+
+    def __create_nixiedocs(self):
+        self.nixiedocuments = {}
+        self.nixiedocuments['BrainDump (unedited)'] = "https://docs.google.com/document/d/1Z6sOjoxhYXBoXydnI3ba8VWQys3T45Rd836TzNq6yZw/edit?usp=sharing"
+        self.nixiedocuments['Community Plan'] = "https://docs.google.com/document/d/1TBQwaRhjDgFLi1xDmQfgY0OMyxhhUDLeKXZzv_NH_88/edit?usp=sharing"
+        self.nixiedocuments['Linux Myths'] = "https://docs.google.com/document/d/1EF7D6iRLkVvK0NwRzp_KOOKABfqrJf0u023KBPRshts/edit?usp=sharing"
+        self.nixiedocuments['Civic Involvement Brain Dump'] = "https://docs.google.com/document/d/1yGD1LK0fTnOLtor5xkIhZtTW02hqrgBkpMQqRU9OcF8/edit?usp=sharing"
+        self.nixiedocuments['github'] = "https://github.com/Nixie-Inc/FOSSCommunity/wiki"
+    def _tinyurl(self, url):
+        return t.create_one(url)
+
+    def nixiecontact(self, irc, msg, args):
+        """
+          Returns the nixiecontact information
+        """
+
+        irc.reply("Twitter:  twitter.com/nixiepixel", private=True)
+        irc.reply("Facebook: http://fb.me/nixiepixel", private=True)
+        irc.reply("Google+: http://google.me/+NixiePixel", private=True)
+        irc.reply("Patreon: http://patreon.com/nixiepixel", private=True)
+        irc.reply("Steam group: http://steamcommunity.com/groups/nixiepixel", private=True)
+    nixiecontact = wrap(nixiecontact)
+
+
+    def nixiedocs(self, irc, msg, args):
+        """
+          Returns the set of collaborative documents created by the community.
+        """
+        for key in self.nixiedocuments:
+            irc.reply(key + ": " + self._tinyurl(self.nixiedocuments[key]), private=True)
+        irc.reply("Github 'cause why note: https://github.com/Nixie-Inc/FOSSCommunity/wiki", private=True)
+    nixiedocs = wrap(nixiedocs)
+  
+
+    def nixiefeed(self, irc, msg, args):
+        """
+        returns top 5 entires of nixie's website RSS feed.
+
+        """
+        print args
+        feed = feedparser.parse(self.nixie_rss)
+        items = feed['entries']
+        count =0
+        for item in items:
+            if count >= 5:
+                break;
+            line = item['published'] + ":  " + item['title'] + " ::: " + item['id']
+            irc.reply(line, private=True)
+            count += 1
+
+    nixiefeed  = wrap(nixiefeed)
+
+Class = Nixie
+
+
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab textwidth=79:
